dc0753f8ac93d1b54331cab847eb76019f880d83
annles <ww>
2022-05-26 20:35:13 +0300

    cache done

---
+++ docstring.py
@@ -0,0 +1,21 @@
+import json
+
+from faker import Faker
+from config import ARGUM1
+
+print(ARGUM1 +10)
+
+
+
+def test(arg1, arg2):
+    """
+    This function...
+    :param arg1: ARGUM1 ...
+    :param arg2:
+    :return:
+    """
+    ...
+
+print(test.__name__)
+print(test.__doc__)
+
